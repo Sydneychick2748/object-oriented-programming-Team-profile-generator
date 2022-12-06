b92f7688ac52acc459d0bcffcf7a1e5b0919bc80
ana <ana@anniecannons.com>
2022-12-06 08:16:50 -0800

    my functions were not working and so i changed my questions to arrays to see if this might work for me

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// const { default: inquirer } = require("inquirer")
-const generateTemplate = require("./src/generateTemplate");
-
+// Import classes
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Import HTML templates
+const generateTemplate = require("./src/generateTemplate");
+const generateManagerCard = require("./src/generateTemplate");
+const generateEngineerCard = require("./src/generateTemplate");
+const generateInternCard = require("./src/generateTemplate");
 
 // bring in the requirer
 const fs = require("fs");
@@ -12,173 +17,175 @@ const inquirer = require("inquirer");
 // employee =[{manage }, {intern }, {engineer}] push the class into here
 const employeeArray = [];
 
-// create a function tht runs required questions  .then don't let them end then run another prompt .then (() =>inquirer.prompt()then)
+// Add manager
+const addManager = [
+  {
+    name: "role",
+    type: "confirm",
+    message: "This is your team generator!!! Are you ready to make a Great Working Team?",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "Please enter the name of the manager:",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "What is your employee ID?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please enter your email address:",
+  },
+  {
+    name: "officeNumber",
+    type: "input",
+    message: "What is your office number?",
+  },
+  {
+    name: "addMore",
+    type: "list",
+    message: "What would you like to do next?",
+    choices: ["Add Engineer", "Add Intern", "My team is complete!"],
+  },
+];
 
-const addManager = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is  the managers name of this team ?",
-      },
+// Add new engineer
+const addEngineer = [
+  {
+    name: "name",
+    type: "input",
+    message: "Please enter the name of the engineer:",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "Please enter the ID of the engineer:",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is the engineer's email address?",
+  },
+  {
+    name: "github",
+    type: "input",
+    message: "Please enter the engineer's Github username:",
+  },
+  {
+    name: "addMore",
+    type: "list",
+    message: "What would you like to do next?",
+    choices: ["Add Engineer", "Add Intern", "My team is complete!"],
+  },
+];
 
-      {
-        type: "input",
-        name: "id",
-        message: "Please enter the manager's ID number ?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the manager's email address?",
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is  the  manager's office number ?",
-      },
-    ])
-    .then((managerInput) => {
-      const { name, id, email, officeNumber } = managerInput;
-      const manager = new Manager(name, id, email, officeNumber);
+// Add intern
+const addIntern = [
+  {
+    name: "name",
+    type: "input",
+    message: "What is the intern's name?",
+  },
+  {
+    name: "id",
+    type: "input",
+    message: "What is the intern's employee ID?",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please enter the intern's email address:",
+  },
+  {
+    name: "school",
+    type: "input",
+    message: "What college or university does the intern attend?",
+  },
+  {
+    name: "addMore",
+    type: "list",
+    message: "What would you like to do next?",
+    choices: ["Add Engineer", "Add Intern", "My team is complete!"],
+  },
+];
 
-      employeeArray.push(manager);
-      console.log(manager);
-    });
-};
-const addEmployee = () => {
-  return (
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "role",
-          message: "Please choose your employee's role",
-          choices: ["Engineer", "Intern", "im am done adding a team members "],
-        },
-        {
-          type: "input",
-          name: "name",
-          message: "What's the name of the employee?",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Please enter the employee's ID.",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Please enter the employee's email.",
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "Please enter the employee's github username.",
-          when: (input) => input.role === "Engineer",
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "Please enter the intern's school",
-          when: (input) => input.role === "Intern",
-        },
+// Initialize application
+ask(addManager);
 
-        //  ====================================== do i need this to do the loop or just the above code ??
-        {
-          type: "input",
-          name: "finished",
-          message: "i am done adding members",
-          when: (input) => input.role === "finished",
-        },
-      ])
-      // if (answer.employee === 'I am done adding team members.') {
-      //     console.log(employeeList);
-      //     return employeeList;
-      // }
-      // })
+//  questions if member needs to be added
+function ask(questionArr) {
+  inquirer
+    .prompt(questionArr)
+    .then((member) => {
+      employeeArray.push(member);
 
-      .then((employeeData) => {
-        // data for employee types
+      if (member.addMore === " Add Engineer") {
+        ask(addEngineer);
+      } else if (member.addMore === " Add Intern") {
+        ask(addIntern);
+      } else {
+        createProfiles(employeeArray);
+      }
+    })
+    .catch((err) => console.log(err));
+}
 
-        let { name, id, email, role, github, school } = employeeData;
-        let employee;
+// create the team in the array
+function createProfiles(employeeArray) {
+  const profiles = employeeArray.map((member) => {
+    const { name, id, email } = member;
 
-        if (role === "Engineer") {
-          employee = new Engineer(name, id, email, github);
+    // If you're adding a manager, ask for office number
+    if (member.hasOwnProperty("officeNumber")) {
+      const { officeNumber } = member;
+      return new Manager(name, id, email, officeNumber);
+    }
 
-          console.log(employee);
-        } else if (role === "Intern") {
-          employee = new Intern(name, id, email, school);
+    // if you're adding an engineer, as for github
+    if (member.hasOwnProperty("github")) {
+      const { github } = member;
+      return new Engineer(name, id, email, github);
+    }
 
-        //   console.log(employee);
-        // } else if (role === "finished") {
-        //   return employeeArray;
-        // }
-        // else if (role === "im am done adding a team members") {
-        //     return employeeArray;
-        }
-        //     console.log(employee);
-        // }
-        console.log(employeeArray);
-        // =============================================== why is my loop still running what am i missing ??
-        employeeArray.push(employee);
-
-        if (role) {
-          return addEmployee(employeeArray);
-        } else if (role === "im am done adding a team members ") {
-            return generateTemplate(employeeArray);
-    
-        }
-      })
-  );
-};
-
-addManager()
-  .then(addEmployee)
-  .then((employeeArray) => {
-    return generateTemplate(employeeArray);
-  })
-  .then(() => {
-    return writeFile();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
-//     err ? console.log(err) : console.log("Your team profile has been  created!")
-// );
-
-//     err ? console.log(err) : console.log('Successfully created index.html!')
-// );
-
-// then loop threw the array above and convert it to html with a template lit
-// function to generate HTML page file using file system
-const writeFile = (data) => {
-  fs.writeFile("./dist/index.html", data, (err) => {
-    // if there is an error
-    if (err) {
-      console.log(err);
-      return;
-      // when the profile has been created
-    } else {
-      console.log("Your team profile has been  created!");
+    // if you're adding an intern, ask for school
+    if (member.hasOwnProperty("school")) {
+      const { school } = member;
+      return new Intern(name, id, email, school);
     }
   });
-};
 
-// addManager()
-//   .then(addEmployee)
-//   .then(employeeArray => {
+  // Generate HTML from the profiles made
+  generateHtml(profiles);
+}
 
-//     return  generateTemplate(employeeArray);
+//   generating the profiles into the cards html
+function generateHtml(profiles) {
+  let profileCards = "";
+  profiles.forEach((profile) => {
+    if (profile instanceof Manager) {
+      const card = generateManagerCard(profile);
+      profileCards += card;
+    } else if (profile instanceof Engineer) {
+      const card = generateEngineerCard(profile);
+      profileCards += card;
+    } else if (profile instanceof Intern) {
+      const card = generateInternCard(profile);
+      profileCards += card;
+    }
+  });
 
-//   })
-//   .then(indexHTML => {
-//     return writeFile(indexHTML);
-//   })
-//   .catch(err => {
-//  console.log(err);
-//   });
+  const newHtml = generateTemplate(profileCards);
+
+  writeHtml(newHtml);
+}
+
+// Function to write the final HTML document in dist folder
+function writeHtml(newHtml) {
+  fs.writeFile("./dist/team-profile.html", newHtml, (err) => {
+    if (err) throw err;
+    console.log("HTML document successfully created in the /dist folder.");
+  });
+}
